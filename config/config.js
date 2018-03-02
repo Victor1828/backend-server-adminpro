67d911a08ba2b7f1b795c431383b44573a94e694
Victor Vega <victorvega1828@gmail.com>
2018-03-01 23:43:11 -0600

    archivos config eliminados

--- config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports.SEED = 'Amegalnov1';
-
-// Google
-module.exports.GOOGLE_CLIENT = '928276657987-0p2accje6vbdhguoockha4enfc026hi8.apps.googleusercontent.com';
-module.exports.GOOGLE_SECRET_KEY = '3gtFPMivbYZ64A96MsiXFWXZ';
